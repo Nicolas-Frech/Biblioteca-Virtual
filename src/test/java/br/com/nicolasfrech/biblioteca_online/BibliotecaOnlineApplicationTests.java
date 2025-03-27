745d5177980f982af9846097bd4032a1a3346eaa
Nicolas Frech <nicolasfrechdev1@gmail.com>
2025-03-27 13:17:29 -0300

    🚀 Initial commit

---
+++ src/test/java/br/com/nicolasfrech/biblioteca_online/BibliotecaOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nicolasfrech.biblioteca_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
