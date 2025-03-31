423392fc67303348f90b06aaa68e51b870db7d35
Nicolas Frech <nicolasfrechdev1@gmail.com>
2025-03-31 17:48:37 -0300

    :art: style: change index.html apperance

--- src/main/java/br/com/nicolasfrech/biblioteca_online/infra/book/persistence/BookRepositoryJPA.java
+++ src/main/java/br/com/nicolasfrech/biblioteca_online/infra/book/persistence/BookRepositoryJPA.java
@@ -1,7 +1,5 @@
 package br.com.nicolasfrech.biblioteca_online.infra.book.persistence;
 
-import br.com.nicolasfrech.biblioteca_online.domain.book.Book;
-import br.com.nicolasfrech.biblioteca_online.infra.book.gateway.BookRepositoryImpl;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Pageable;
@@ -10,8 +8,6 @@ public interface BookRepositoryJPA extends JpaRepository<BookEntity, Long> {
     boolean existsByIdAndActiveTrue(Long id);
 
     boolean existsByTitleAndActiveTrue(String title);
-
-    BookEntity findByTitle(String title);
 
     Page<BookEntity> findAllByActiveTrue(Pageable pagination);
 
